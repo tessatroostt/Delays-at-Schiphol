a25a0f4195b61ee221775d488f2822bc99dec6e4
tessatroostt <95234078+tessatroostt@users.noreply.github.com>
2021-12-13 21:36:27 +0100

    Update code.py

--- code.py
+++ code.py
@@ -95,7 +95,7 @@ if sidebar_keuze == 'Flights vs Covid':
   
   with col1:
     fig = px.line(flights, x="Date", y=['Europe', 'Intercontinental',"Total"], title='Amount of flights at Schiphol Airport Amsterdam 2018-2021',
-             color_discrete_map = {'Europe': 'rgb(220, 176, 242)', 'Intercontinental': 'rgb(158, 185,243)', 'Total': 'rgb(254, 136, 177)'}).update_layout(width = 1000)
+             color_discrete_map = {'Europe': 'rgb(220, 176, 242)', 'Intercontinental': 'rgb(158, 185,243)', 'Total': 'rgb(254, 136, 177)'}).update_layout(width = 800)
     st.write(fig)
   
   with col2:
